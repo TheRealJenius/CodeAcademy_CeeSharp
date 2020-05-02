17f3c46390f0fdc18c8c05ee39bbc8efefa8c3e0
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-02 18:39:01 +0100

    Learning to convert strings

---
+++ convert_string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace convert_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for fave number
+            Console.Write("Enter your favorite number!: ");
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            // Turn that answer into an int
+        }
+    }
+}
