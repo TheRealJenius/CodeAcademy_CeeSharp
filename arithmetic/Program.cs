84729fefdb283c83efcf090a6e152837368d1983
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-02 20:18:36 +0100

    Practicing arithmetic operations on different data types

---
+++ arithmetic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge = 27;
+
+
+            // Length of years on Jupiter (in Earth years)
+            double jupiterYears = 11.86;
+        
+        
+            // Age on Jupiter
+            double jupiterAge = userAge / jupiterYears;
+        
+        
+            // Time to Jupiter
+            double journeyToJupiter = 6.142466;
+        
+        
+            // New Age on Earth
+            double newEarthAge = userAge + journeyToJupiter;
+        
+        
+            // New Age on Jupiter
+            double newJupiterAge = newEarthAge /jupiterYears;
+        
+        
+            // Log calculations to console
+            Console.WriteLine(userAge);
+            Console.WriteLine(newEarthAge);
+            Console.WriteLine(jupiterAge);
+            Console.WriteLine(newJupiterAge);
+        }
+    }
+}
