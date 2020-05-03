dae0eb10d218e17b233c50e3d99cae3856dcb355
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-03 17:54:22 +0100

    Practicing switch and ternary statements

---
+++ MovieSwitch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MovieSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a genre any genre:");
+            string genre = Console.ReadLine();
+
+            switch (genre)
+            {
+              case "Drama":
+                Console.WriteLine("Citizen Kane");
+                break;
+
+              case "Comedy":
+                Console.WriteLine("Duck Soup");
+                break;
+
+              case "Adventure":
+                Console.WriteLine("King Kong");
+                break;
+
+              case "Horror":
+                Console.WriteLine("Psycho");
+                break;
+
+              case "Science Fiction":
+                Console.WriteLine("2001: A Space Odyssey");
+                break;
+
+              default:
+                Console.WriteLine("No movie found");
+                break;
+            }
+
+            Console.WriteLine("Do you like the suggestion: ");
+            string answer = Console.ReadLine();
+            string display = (answer == "yes") ? "That's Great" : "Well too bad";
+            Console.WriteLine(display);
+        }
+    }
+}
