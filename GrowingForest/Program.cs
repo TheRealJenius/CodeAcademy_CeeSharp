99ca9dbbf641a99d3d27f92cfce25c0608263f52
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-13 19:05:51 +0100

    Learning the basics about classes and objects

---
+++ GrowingForest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest("Amazon");
+      
+      Console.WriteLine(f.Trees);
+      f.Grow();
+      Console.WriteLine(f.Trees);
+
+    }
+  }
+}
